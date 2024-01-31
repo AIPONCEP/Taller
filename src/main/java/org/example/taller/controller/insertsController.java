f4b6849aaa30a699b6d2f6c91a6c5a8186d56995
AIPONCEP <ana_ponce_perez@hotmail.es>
2024-01-31 22:15:55 +0100

    arreglado el insert

--- src/main/java/org/example/taller/controller/insertsController.java
+++ src/main/java/org/example/taller/controller/insertsController.java
@@ -18,31 +18,15 @@ public class insertsController {
     public TextField tFcalle;
     public TextField tFciudad;
     public TextField tFcp;
-/*
-   public Cliente(String nombre, Integer num, String calle, String ciudad, String cp, Integer numCoches, String tlfs) {
-        super(nombre, num, calle, ciudad, cp);
-        this.tlfs = Arrays.toString(convertirString(tlfs));
-        this.numCoches = numCoches;
-    }
- */
+
     public void insertar(MouseEvent mouseEvent) {
         Cliente cliente=new Cliente(
                 tFnombre.getText(),Integer.parseInt(tfnumDirec.getText()),
                 tFcalle.getText(),tFciudad.getText(),tFcp.getText(),
                 Integer.parseInt(tFnumc.getText()),tFtlf.getText()
         );
-        String[] values=new String[7];
-        values[0]= cliente.getNombre();
-        values[1]= String.valueOf(cliente.getNum());
-        values[2]= cliente.getCalle();
-        values[3]= cliente.getCiudad();
-        values[4]= cliente.getCp();
-        values[5]= String.valueOf(cliente.getNumCoches());
-        values[6]= cliente.getTlfs();
-
-        DBManager.actualizarDML("insert into clientes(persona,numcoches, tlf) values ((?, (?,?,?,?)),?,?);",values);
+        DBManager.insertarClientes(cliente,tFnombre.getText(),Integer.parseInt(tfnumDirec.getText()),tFcalle.getText(),tFciudad.getText(),tFcp.getText(),Integer.parseInt(tFnumc.getText()),tFtlf.getText());
     }
-
     public void volver(MouseEvent mouseEvent) {
         try {
             Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/org/example/taller/Main-view.fxml")));
--- src/main/java/org/example/taller/model/DBManager.java
+++ src/main/java/org/example/taller/model/DBManager.java
@@ -6,48 +6,50 @@ public class DBManager {
     private static Connection con = LocalConnection.getConnection();
     private static PreparedStatement sentencia = null;
     private static ResultSet resultado = null;
-    public static String mostrarClientes(){
-            con=LocalConnection.getConnection();
-            if(con!=null){
-                try {
-                    sentencia = con.prepareStatement("Select idcliente," +
-                            "(persona::persona_type).nombre," +
-                            "(((persona::persona_type).direccion::direccion_type).num),"+
-                            "(((persona::persona_type).direccion::direccion_type).calle),"+
-                            "(((persona::persona_type).direccion::direccion_type).ciudad),"+
-                            "(((persona::persona_type).direccion::direccion_type).cp),"+
-                            " numcoches,tlf from clientes;");
-                    resultado = sentencia.executeQuery();
-                    ResultSetMetaData metaData = resultado.getMetaData();
-                    int columnCount = metaData.getColumnCount();
-                    StringBuilder result = new StringBuilder();
-                    while (resultado.next()) {
-                        StringBuilder row = new StringBuilder();
-                        String valuetlfformat;
-                        for (int i = 1; i <= columnCount; i++) {
-                            String columnName = metaData.getColumnName(i);
-                            Object value = resultado.getObject(columnName);
-                            if(columnName.contains("tlf")){
-                                valuetlfformat=  value.toString().substring(1,value.toString().length()-1);
-                                row.append(columnName).append(":").append(valuetlfformat).append("-");
-                            }else{
-                                row.append(columnName).append(":").append(value).append("-");
-                            }
+
+    public static String mostrarClientes() {
+        con = LocalConnection.getConnection();
+        if (con != null) {
+            try {
+                sentencia = con.prepareStatement("Select idcliente," +
+                        "(persona::persona_type).nombre," +
+                        "(((persona::persona_type).direccion::direccion_type).num)," +
+                        "(((persona::persona_type).direccion::direccion_type).calle)," +
+                        "(((persona::persona_type).direccion::direccion_type).ciudad)," +
+                        "(((persona::persona_type).direccion::direccion_type).cp)," +
+                        " numcoches,tlf from clientes;");
+                resultado = sentencia.executeQuery();
+                ResultSetMetaData metaData = resultado.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                StringBuilder result = new StringBuilder();
+                while (resultado.next()) {
+                    StringBuilder row = new StringBuilder();
+                    String valuetlfformat;
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnName(i);
+                        Object value = resultado.getObject(columnName);
+                        if (columnName.contains("tlf")) {
+                            valuetlfformat = value.toString().substring(1, value.toString().length() - 1);
+                            row.append(columnName).append(":").append(valuetlfformat).append("-");
+                        } else {
+                            row.append(columnName).append(":").append(value).append("-");
                         }
-                        result.append(row).append("\n");
                     }
-                    LocalConnection.closeConnection();
-                    sentencia.close();
-                    return result.toString();
-                } catch(SQLException e){
-                    System.out.println(e.getMessage());
+                    result.append(row).append("\n");
                 }
+                LocalConnection.closeConnection();
+                sentencia.close();
+                return result.toString();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
             }
-            return null;
+        }
+        return null;
     }
-    public static String mostrarMecanicos(){
-        con=LocalConnection.getConnection();
-        if(con!=null){
+
+    public static String mostrarMecanicos() {
+        con = LocalConnection.getConnection();
+        if (con != null) {
             try {
                 sentencia = con.prepareStatement("Select " +
                         "idmecanico, " +
@@ -57,7 +59,7 @@ public class DBManager {
                         "((empleados::empleados_type).persona.nombre)," +
                         "((empleados::empleados_type).persona.direccion.num)," +
                         "((empleados::empleados_type).persona.direccion.calle)," +
-                         "((empleados::empleados_type).persona.direccion.ciudad)," +
+                        "((empleados::empleados_type).persona.direccion.ciudad)," +
                         "((empleados::empleados_type).persona.direccion.cp)," +
                         "(empleados::empleados_type).pass," +
                         "(empleados::empleados_type).nomina," +
@@ -79,39 +81,33 @@ public class DBManager {
                 LocalConnection.closeConnection();
                 sentencia.close();
                 return result.toString();
-            } catch(SQLException e){
+            } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         }
         return null;
     }
 
-    public static boolean actualizarDML(String sql, Object[] values){
-        con = LocalConnection.getConnection();
-        if (con != null) {
-            try {
-                sentencia = con.prepareStatement(sql);
-                for (int i = 0; i < values.length; i++) {
-                    Object value = values[i];
-                    sentencia.setString(i + 1, String.valueOf(value));
-                }
-                int filasAfectadas = sentencia.executeUpdate();
-                System.out.println("Filas afectadas: " + filasAfectadas);
-                return true;
-            } catch (SQLException e) {
-                System.out.println("Error: " + e.getMessage());
-                return false;
-            } finally {
-                if (sentencia != null) {
-                    try {
-                        sentencia.close();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-                LocalConnection.closeConnection();
-            }
+    public static boolean insertarClientes(Cliente cliente, String nombre, Integer num, String calle, String ciudad, String cp, Integer numCoches, String tlfs) {
+        try (Connection con = LocalConnection.getConnection();
+             PreparedStatement sentencia = con.prepareStatement("INSERT INTO clientes (persona, numcoches, tlf) VALUES (ROW(?, ROW(?, ?, ?, ?)), ?, ?)")) {
+
+            // Establecer valores en la consulta
+            sentencia.setString(1, cliente.getNombre());
+            sentencia.setInt(2, cliente.getNum());
+            sentencia.setString(3, cliente.getCalle());
+            sentencia.setString(4, cliente.getCiudad());
+            sentencia.setString(5, cliente.getCp());
+            sentencia.setInt(6, cliente.getNumCoches());
+            sentencia.setArray(7, con.createArrayOf("VARCHAR", cliente.convertirString(tlfs)));
+
+            int filasAfectadas = sentencia.executeUpdate();
+            System.out.println("Filas afectadas: " + filasAfectadas);
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            return false;
         }
-        return false;
     }
 }
